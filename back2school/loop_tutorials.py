78f71f8840b3737fb6de818af8e5e89ed6490487
Reuben Brasher <rkbrasher@gmail.com>
2024-04-13 13:41:26 -0700

    Enforce coding style with black formatter (#2)

--- back2school/loop_tutorials.py
+++ back2school/loop_tutorials.py
@@ -1,18 +1,22 @@
 def for_loop():
-    for i in range (10):
+    for i in range(10):
         print(i)
+
 
 def for_asdf():
     for a in "asdf":
         print(a)
 
+
 def for_foo():
-    for a in [0,1,2,"foo","bar"]:
+    for a in [0, 1, 2, "foo", "bar"]:
         print(a)
+
 
 def for_range():
     for a in range(5, 10, 2):
         print(a)
+
 
 def for_list_total():
     total = 0
@@ -20,10 +24,12 @@ def for_list_total():
         total += cost
     print(f"Total: ${total}")
 
+
 def for_xy_coord_gen():
     for x in range(m):
         for y in range(n):
-            print(f'({x}, {y})')
+            print(f"({x}, {y})")
+
 
 """ prices = [10, 20, 61, 309]
 for_list_total() """
@@ -31,9 +37,3 @@ for_list_total() """
 m = 4
 n = 3
 for_xy_coord_gen()
-
-
-
-
-
-
