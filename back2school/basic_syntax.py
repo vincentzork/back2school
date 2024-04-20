2d46d00633cfaffd7bcba788143ee23d51c8d2e7
Vincent Chang <vincent.chang167@gmail.com>
2024-04-20 09:49:59 -0700

    added the third parft of instructional code

--- back2school/basic_syntax.py
+++ back2school/basic_syntax.py
@@ -7,7 +7,7 @@ for number in numbers:
     squares.append(number ** 2)  # Append the square of each number to the list
 
 print(squares)  # Output: [1, 4, 9, 16, 25]
-"""
+
 
 # Python code to demonstrate variable assignment and data types
 age = 30  # An integer assignment
@@ -21,3 +21,21 @@ print(f"{name} is {age} years old and {height} feet tall. Adult? {is_adult}")
 # List of mixed data types
 mixed_list = [age, height, name, is_adult]
 print(mixed_list)  # Output: [30, 5.9, 'Alex', True]
+
+"""
+# A list of crayons
+crayons = ["blue", "green", "yellow", "red"]
+
+for crayon in crayons:
+    if crayon == "blue":
+        print("Color the sky with", crayon)
+    elif crayon == "green":
+        print("Color the grass with", crayon)
+    else:
+        print("Color the flowers with", crayon)
+
+# This will tell you:
+# Color the sky with blue
+# Color the grass with green
+# Color the flowers with yellow
+# Color the flowers with red
