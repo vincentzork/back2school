14cff6dcd7088039984ef5b734e41ac7a8a34905
Reuben Brasher <rkbrasher@gmail.com>
2024-05-25 15:19:04 -0700

    Instantiate devils

--- back2school/devil.py
+++ back2school/devil.py
@@ -48,27 +48,27 @@ asmodeus_custom_stat_block = {
 }
 
 baalzebul_custom_stat_block = {
-    "Name": "Baalzebul",
-    "Hit Points": 450,
-    "Armor Class": 24,
-    "Speed": "40 ft, fly 60 ft",
-    "Abilities": {
-        "Strength": 22,
-        "Dexterity": 18,
-        "Constitution": 24,
-        "Intelligence": 28,
-        "Wisdom": 22,
-        "Charisma": 30,
+    "name": "Baalzebul",
+    "hit_points": 450,
+    "armor_class": 24,
+    "speed": "40 ft, fly 60 ft",
+    "abilities": {
+        "strength": 22,
+        "dexterity": 18,
+        "constitution": 24,
+        "intelligence": 28,
+        "wisdom": 22,
+        "charisma": 30,
     },
-    "Saving Throws": {
-        "STR": "+13",
-        "DEX": "+11",
-        "CON": "+14",
-        "INT": "+16",
-        "WIS": "+13",
-        "CHA": "+18",
+    "saving_throws": {
+        "strength": "+13",
+        "dexterity": "+11",
+        "constitution": "+14",
+        "intelligence": "+16",
+        "wisdom": "+13",
+        "charisma": "+18",
     },
-    "Skills": [
+    "skills": [
         "Arcana +16",
         "Deception +18",
         "Insight +13",
@@ -76,20 +76,20 @@ baalzebul_custom_stat_block = {
         "Perception +13",
         "Persuasion +18",
     ],
-    "Damage Resistances": "cold; bludgeoning, piercing, and slashing from nonmagical attacks",
-    "Damage Immunities": "fire, poison",
-    "Condition Immunities": "charmed, exhausted, frightened, poisoned",
-    "Senses": "truesight 120 ft., passive Perception 23",
-    "Languages": "all, telepathy 120 ft.",
-    "Challenge": 29,
-    "Special Abilities": [
+    "damage_resistances": "cold; bludgeoning, piercing, and slashing from nonmagical attacks",
+    "damage_immunities": "fire, poison",
+    "condition_immunities": "charmed, exhausted, frightened, poisoned",
+    "senses": "truesight 120 ft., passive Perception 23",
+    "languages": "all, telepathy 120 ft.",
+    "challenge": 29,
+    "special_abilities": [
         "Innate Spellcasting",
         "Magic Resistance",
         "Corrupting Touch",
         "Hellish Rejuvenation",
         "Legendary Actions",
     ],
-    "Legendary Actions": {
+    "legendary_actions": {
         "Infernal Command": "Baalzebul commands his minions with unparalleled authority."
     },
 }
@@ -325,6 +325,241 @@ geryon_custom_stat_block = {
         "Legendary Actions",
     ],
     "Legendary Actions": {
+        "Infernal Command": "Geryon commands his minions with unparalleled authority."
+    },
+}
+
+belial_custom_stat_block = {
+    "name": "Belial",
+    "hit_points": 480,
+    "armor_class": 25,
+    "speed": "40 ft, fly 60 ft",
+    "abilities": {
+        "strength": 24,
+        "dexterity": 18,
+        "constitution": 25,
+        "intelligence": 26,
+        "wisdom": 22,
+        "charisma": 28,
+    },
+    "saving_throws": {
+        "strength": "+14",
+        "dexterity": "+11",
+        "constitution": "+15",
+        "intelligence": "+15",
+        "wisdom": "+13",
+        "charisma": "+17",
+    },
+    "skills": [
+        "Arcana +15",
+        "Deception +17",
+        "Insight +13",
+        "Intimidation +17",
+        "Perception +13",
+        "Persuasion +17",
+    ],
+    "damage_resistances": "cold; bludgeoning, piercing, and slashing from nonmagical attacks",
+    "damage_immunities": "fire, poison",
+    "condition_immunities": "charmed, exhausted, frightened, poisoned",
+    "senses": "truesight 120 ft., passive Perception 23",
+    "languages": "all, telepathy 120 ft.",
+    "challenge": 28,
+    "special_abilities": [
+        "Innate Spellcasting",
+        "Magic Resistance",
+        "Fire Aura",
+        "Infernal Command",
+        "Legendary Actions",
+    ],
+    "legendary_actions": {
+        "Infernal Command": "Belial commands his minions with unparalleled authority."
+    },
+}
+
+dispater_custom_stat_block = {
+    "name": "Dispater",
+    "hit_points": 470,
+    "armor_class": 25,
+    "speed": "40 ft, fly 60 ft",
+    "abilities": {
+        "strength": 22,
+        "dexterity": 18,
+        "constitution": 24,
+        "intelligence": 30,
+        "wisdom": 22,
+        "charisma": 28,
+    },
+    "saving_throws": {
+        "strength": "+13",
+        "dexterity": "+11",
+        "constitution": "+14",
+        "intelligence": "+17",
+        "wisdom": "+13",
+        "charisma": "+17",
+    },
+    "skills": [
+        "Arcana +17",
+        "Deception +17",
+        "Insight +13",
+        "Intimidation +17",
+        "Perception +13",
+        "Persuasion +17",
+    ],
+    "damage_resistances": "cold; bludgeoning, piercing, and slashing from nonmagical attacks",
+    "damage_immunities": "fire, poison",
+    "condition_immunities": "charmed, exhausted, frightened, poisoned",
+    "senses": "truesight 120 ft., passive Perception 23",
+    "languages": "all, telepathy 120 ft.",
+    "challenge": 28,
+    "special_abilities": [
+        "Innate Spellcasting",
+        "Magic Resistance",
+        "Lord of Dis",
+        "Infernal Regeneration",
+        "Legendary Actions",
+    ],
+    "legendary_actions": {
+        "Infernal Command": "Dispater commands his minions with unparalleled authority."
+    },
+}
+
+mammon_custom_stat_block = {
+    "name": "Mammon",
+    "hit_points": 460,
+    "armor_class": 24,
+    "speed": "40 ft, fly 60 ft",
+    "abilities": {
+        "strength": 22,
+        "dexterity": 18,
+        "constitution": 24,
+        "intelligence": 28,
+        "wisdom": 22,
+        "charisma": 30,
+    },
+    "saving_throws": {
+        "strength": "+13",
+        "dexterity": "+11",
+        "constitution": "+14",
+        "intelligence": "+16",
+        "wisdom": "+13",
+        "charisma": "+18",
+    },
+    "skills": [
+        "Arcana +16",
+        "Deception +18",
+        "Insight +13",
+        "Intimidation +18",
+        "Perception +13",
+        "Persuasion +18",
+    ],
+    "damage_resistances": "cold; bludgeoning, piercing, and slashing from nonmagical attacks",
+    "damage_immunities": "fire, poison",
+    "condition_immunities": "charmed, exhausted, frightened, poisoned",
+    "senses": "truesight 120 ft., passive Perception 23",
+    "languages": "all, telepathy 120 ft.",
+    "challenge": 28,
+    "special_abilities": [
+        "Innate Spellcasting",
+        "Magic Resistance",
+        "Golden Gaze",
+        "Greed's Reward",
+        "Legendary Actions",
+    ],
+    "legendary_actions": {
+        "Infernal Command": "Mammon commands his minions with unparalleled authority."
+    },
+}
+
+mephistopheles_custom_stat_block = {
+    "name": "Mephistopheles",
+    "hit_points": 500,
+    "armor_class": 26,
+    "speed": "40 ft, fly 60 ft",
+    "abilities": {
+        "strength": 24,
+        "dexterity": 18,
+        "constitution": 26,
+        "intelligence": 30,
+        "wisdom": 22,
+        "charisma": 32,
+    },
+    "saving_throws": {
+        "strength": "+14",
+        "dexterity": "+11",
+        "constitution": "+15",
+        "intelligence": "+17",
+        "wisdom": "+13",
+        "charisma": "+19",
+    },
+    "skills": [
+        "Arcana +17",
+        "Deception +19",
+        "Insight +14",
+        "Intimidation +19",
+        "Perception +14",
+        "Persuasion +19",
+    ],
+    "damage_resistances": "cold; bludgeoning, piercing, and slashing from nonmagical attacks",
+    "damage_immunities": "fire, poison",
+    "condition_immunities": "charmed, exhausted, frightened, poisoned",
+    "senses": "truesight 120 ft., passive Perception 24",
+    "languages": "all, telepathy 120 ft.",
+    "challenge": 30,
+    "special_abilities": [
+        "Innate Spellcasting",
+        "Magic Resistance",
+        "Hellfire Aura",
+        "Infernal Rejuvenation",
+        "Legendary Actions",
+    ],
+    "legendary_actions": {
+        "Infernal Command": "Mephistopheles commands his minions with unparalleled authority."
+    },
+}
+
+geryon_custom_stat_block = {
+    "name": "Geryon",
+    "hit_points": 480,
+    "armor_class": 25,
+    "speed": "40 ft, fly 60 ft",
+    "abilities": {
+        "strength": 24,
+        "dexterity": 18,
+        "constitution": 26,
+        "intelligence": 22,
+        "wisdom": 24,
+        "charisma": 28,
+    },
+    "saving_throws": {
+        "strength": "+14",
+        "dexterity": "+11",
+        "constitution": "+15",
+        "intelligence": "+13",
+        "wisdom": "+14",
+        "charisma": "+17",
+    },
+    "skills": [
+        "Arcana +13",
+        "Deception +17",
+        "Insight +14",
+        "Intimidation +17",
+        "Perception +14",
+        "Persuasion +17",
+    ],
+    "damage_resistances": "cold; bludgeoning, piercing, and slashing from nonmagical attacks",
+    "damage_immunities": "fire, poison",
+    "condition_immunities": "charmed, exhausted, frightened, poisoned",
+    "senses": "truesight 120 ft., passive Perception 24",
+    "languages": "all, telepathy 120 ft.",
+    "challenge": 29,
+    "special_abilities": [
+        "Innate Spellcasting",
+        "Magic Resistance",
+        "Fear Aura",
+        "Infernal Regeneration",
+        "Legendary Actions",
+    ],
+    "legendary_actions": {
         "Infernal Command": "Geryon commands his minions with unparalleled authority."
     },
 }
--- back2school/tests/test_devil.py
+++ back2school/tests/test_devil.py
@@ -30,7 +30,7 @@ from typing import Dict, List
 from pydantic import BaseModel
 
 
-class AsmodeusStatBlock(BaseModel):
+class ArchdevilStatBlock(BaseModel):
     name: str
     hit_points: int
     armor_class: int
@@ -50,8 +50,8 @@ class AsmodeusStatBlock(BaseModel):
 
 @pytest.fixture
 def asmodeus_stat_block():
-    # Fixture to provide a sample instance of AsmodeusStatBlock for testing
-    return AsmodeusStatBlock(**devil.asmodeus_custom_stat_block)
+    # Fixture to provide a sample instance of ArchdevilStatBlock for testing
+    return ArchdevilStatBlock(**devil.asmodeus_custom_stat_block)
 
 
 def test_asmodeus_stat_block(asmodeus_stat_block):
@@ -79,4 +79,210 @@ def test_asmodeus_stat_block(asmodeus_stat_block):
     assert (
         asmodeus_stat_block.legendary_actions["Infernal Command"]
         == "Asmodeus commands his minions with unparalleled authority."
+    )
+
+
+@pytest.fixture
+def baalzebul_stat_block():
+    # Fixture to provide a sample instance of ArchdevilStatBlock for testing
+    return ArchdevilStatBlock(**devil.baalzebul_custom_stat_block)
+
+
+def test_baalzebul_stat_block(baalzebul_stat_block):
+    # Check if the BaalzebulStatBlock instance is created correctly
+    assert baalzebul_stat_block.name == "Baalzebul"
+    assert baalzebul_stat_block.hit_points == 450
+    assert baalzebul_stat_block.armor_class == 24
+    assert baalzebul_stat_block.speed == "40 ft, fly 60 ft"
+    assert baalzebul_stat_block.abilities.strength == 22
+    assert baalzebul_stat_block.saving_throws.strength == 13
+    assert "Deception +18" in baalzebul_stat_block.skills
+    assert (
+        baalzebul_stat_block.damage_resistances
+        == "cold; bludgeoning, piercing, and slashing from nonmagical attacks"
+    )
+    assert baalzebul_stat_block.damage_immunities == "fire, poison"
+    assert (
+        baalzebul_stat_block.condition_immunities
+        == "charmed, exhausted, frightened, poisoned"
+    )
+    assert baalzebul_stat_block.senses == "truesight 120 ft., passive Perception 23"
+    assert baalzebul_stat_block.languages == "all, telepathy 120 ft."
+    assert baalzebul_stat_block.challenge == 29
+    assert "Corrupting Touch" in baalzebul_stat_block.special_abilities
+    assert (
+        baalzebul_stat_block.legendary_actions["Infernal Command"]
+        == "Baalzebul commands his minions with unparalleled authority."
+    )
+
+
+@pytest.fixture
+def belial_stat_block():
+    # Fixture to provide a sample instance of ArchdevilStatBlock for testing
+    return ArchdevilStatBlock(**devil.belial_custom_stat_block)
+
+
+def test_belial_stat_block(belial_stat_block):
+    # Check if the BelialStatBlock instance is created correctly
+    assert belial_stat_block.name == "Belial"
+    assert belial_stat_block.hit_points == 480
+    assert belial_stat_block.armor_class == 25
+    assert belial_stat_block.speed == "40 ft, fly 60 ft"
+    assert belial_stat_block.abilities.strength == 24
+    assert belial_stat_block.saving_throws.strength == 14
+    assert "Deception +17" in belial_stat_block.skills
+    assert (
+        belial_stat_block.damage_resistances
+        == "cold; bludgeoning, piercing, and slashing from nonmagical attacks"
+    )
+    assert belial_stat_block.damage_immunities == "fire, poison"
+    assert (
+        belial_stat_block.condition_immunities
+        == "charmed, exhausted, frightened, poisoned"
+    )
+    assert belial_stat_block.senses == "truesight 120 ft., passive Perception 23"
+    assert belial_stat_block.languages == "all, telepathy 120 ft."
+    assert belial_stat_block.challenge == 28
+    assert "Fire Aura" in belial_stat_block.special_abilities
+    assert (
+        belial_stat_block.legendary_actions["Infernal Command"]
+        == "Belial commands his minions with unparalleled authority."
+    )
+
+
+@pytest.fixture
+def dispater_stat_block():
+    # Fixture to provide a sample instance of ArchdevilStatBlock for testing
+    return ArchdevilStatBlock(**devil.dispater_custom_stat_block)
+
+
+def test_dispater_stat_block(dispater_stat_block):
+    # Check if the DispaterStatBlock instance is created correctly
+    assert dispater_stat_block.name == "Dispater"
+    assert dispater_stat_block.hit_points == 470
+    assert dispater_stat_block.armor_class == 25
+    assert dispater_stat_block.speed == "40 ft, fly 60 ft"
+    assert dispater_stat_block.abilities.strength == 22
+    assert dispater_stat_block.saving_throws.strength == 13
+    assert "Deception +17" in dispater_stat_block.skills
+    assert (
+        dispater_stat_block.damage_resistances
+        == "cold; bludgeoning, piercing, and slashing from nonmagical attacks"
+    )
+    assert dispater_stat_block.damage_immunities == "fire, poison"
+    assert (
+        dispater_stat_block.condition_immunities
+        == "charmed, exhausted, frightened, poisoned"
+    )
+    assert dispater_stat_block.senses == "truesight 120 ft., passive Perception 23"
+    assert dispater_stat_block.languages == "all, telepathy 120 ft."
+    assert dispater_stat_block.challenge == 28
+    assert "Lord of Dis" in dispater_stat_block.special_abilities
+    assert (
+        dispater_stat_block.legendary_actions["Infernal Command"]
+        == "Dispater commands his minions with unparalleled authority."
+    )
+
+
+@pytest.fixture
+def mammon_stat_block():
+    # Fixture to provide a sample instance of ArchdevilStatBlock for testing
+    return ArchdevilStatBlock(**devil.mammon_custom_stat_block)
+
+
+def test_mammon_stat_block(mammon_stat_block):
+    # Check if the MammonStatBlock instance is created correctly
+    assert mammon_stat_block.name == "Mammon"
+    assert mammon_stat_block.hit_points == 460
+    assert mammon_stat_block.armor_class == 24
+    assert mammon_stat_block.speed == "40 ft, fly 60 ft"
+    assert mammon_stat_block.abilities.strength == 22
+    assert mammon_stat_block.saving_throws.strength == 13
+    assert "Deception +18" in mammon_stat_block.skills
+    assert (
+        mammon_stat_block.damage_resistances
+        == "cold; bludgeoning, piercing, and slashing from nonmagical attacks"
+    )
+    assert mammon_stat_block.damage_immunities == "fire, poison"
+    assert (
+        mammon_stat_block.condition_immunities
+        == "charmed, exhausted, frightened, poisoned"
+    )
+    assert mammon_stat_block.senses == "truesight 120 ft., passive Perception 23"
+    assert mammon_stat_block.languages == "all, telepathy 120 ft."
+    assert mammon_stat_block.challenge == 28
+    assert "Golden Gaze" in mammon_stat_block.special_abilities
+    assert (
+        mammon_stat_block.legendary_actions["Infernal Command"]
+        == "Mammon commands his minions with unparalleled authority."
+    )
+
+
+@pytest.fixture
+def mephistopheles_stat_block():
+    # Fixture to provide a sample instance of ArchdevilStatBlock for testing
+    return ArchdevilStatBlock(**devil.mephistopheles_custom_stat_block)
+
+
+def test_mephistopheles_stat_block(mephistopheles_stat_block):
+    # Check if the MephistophelesStatBlock instance is created correctly
+    assert mephistopheles_stat_block.name == "Mephistopheles"
+    assert mephistopheles_stat_block.hit_points == 500
+    assert mephistopheles_stat_block.armor_class == 26
+    assert mephistopheles_stat_block.speed == "40 ft, fly 60 ft"
+    assert mephistopheles_stat_block.abilities.strength == 24
+    assert mephistopheles_stat_block.saving_throws.strength == 14
+    assert "Insight +14" in mephistopheles_stat_block.skills
+    assert (
+        mephistopheles_stat_block.damage_resistances
+        == "cold; bludgeoning, piercing, and slashing from nonmagical attacks"
+    )
+    assert mephistopheles_stat_block.damage_immunities == "fire, poison"
+    assert (
+        mephistopheles_stat_block.condition_immunities
+        == "charmed, exhausted, frightened, poisoned"
+    )
+    assert (
+        mephistopheles_stat_block.senses == "truesight 120 ft., passive Perception 24"
+    )
+    assert mephistopheles_stat_block.languages == "all, telepathy 120 ft."
+    assert mephistopheles_stat_block.challenge == 30
+    assert "Hellfire Aura" in mephistopheles_stat_block.special_abilities
+    assert (
+        mephistopheles_stat_block.legendary_actions["Infernal Command"]
+        == "Mephistopheles commands his minions with unparalleled authority."
+    )
+
+
+@pytest.fixture
+def geryon_stat_block():
+    # Fixture to provide a sample instance of ArchdevilStatBlock for testing
+    return ArchdevilStatBlock(**devil.geryon_custom_stat_block)
+
+
+def test_geryon_stat_block(geryon_stat_block):
+    # Check if the GeryonStatBlock instance is created correctly
+    assert geryon_stat_block.name == "Geryon"
+    assert geryon_stat_block.hit_points == 480
+    assert geryon_stat_block.armor_class == 25
+    assert geryon_stat_block.speed == "40 ft, fly 60 ft"
+    assert geryon_stat_block.abilities.strength == 24
+    assert geryon_stat_block.saving_throws.strength == 14
+    assert "Insight +14" in geryon_stat_block.skills
+    assert (
+        geryon_stat_block.damage_resistances
+        == "cold; bludgeoning, piercing, and slashing from nonmagical attacks"
+    )
+    assert geryon_stat_block.damage_immunities == "fire, poison"
+    assert (
+        geryon_stat_block.condition_immunities
+        == "charmed, exhausted, frightened, poisoned"
+    )
+    assert geryon_stat_block.senses == "truesight 120 ft., passive Perception 24"
+    assert geryon_stat_block.languages == "all, telepathy 120 ft."
+    assert geryon_stat_block.challenge == 29
+    assert "Fear Aura" in geryon_stat_block.special_abilities
+    assert (
+        geryon_stat_block.legendary_actions["Infernal Command"]
+        == "Geryon commands his minions with unparalleled authority."
     )
